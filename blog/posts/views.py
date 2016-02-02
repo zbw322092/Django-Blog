e9f47eebd976e70a64d5d9c97ef0522e9a3679e3
zbw322092 <bz4e14@soton.ac.uk>
2016-02-02 14:32:15 +0800

    update 2
    instance update view, Django messages, delete view, templates and
    inheritance

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -10,6 +11,9 @@ def post_create(request):
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
 		instance.save()
+		#meaage success
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
 
 	#if request.method == "POST":
 	#	print request.POST.get("content")
@@ -33,10 +37,27 @@ def post_list(request): # list items
 		"object_list": queryset,
 		"title": "List"
 	}			
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance = instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+		return HttpResponseRedirect(instance.get_absolute_url())
 
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form": form,
+	}		
+	return render(request, "post_form.html", context)
+
+
+def post_delete(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()	
+	messages.success(request, "Successfully Deleted")
+	return redirect("posts:list")
