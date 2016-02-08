7d1ba2f00508031d90a90fb1a8242f090a4ea50e
zbw322092 <bz4e14@soton.ac.uk>
2016-02-08 22:00:10 +0800

    update 6
    File uploads with FileField and ImageField

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -126,5 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
@@ -57,7 +57,7 @@ def post_list(request):
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance = instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance = instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
